3ff752a06c015111d240945ee66f3058371284d8
zays <ajaydiskets2@gmail.com>
2026-02-15 21:52:32 +0700

    setup gunicorn railway

--- app.py
+++ app.py
@@ -52,6 +52,4 @@ def get_otp(login, domain):
         return jsonify({"otp": None})
 
 
-    if __name__ == "__main__":
-        port = int(os.environ.get("PORT", 5000))
-        app.run(host="0.0.0.0", port=port)
+
